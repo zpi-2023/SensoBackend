ac0455d93a006f072761841c22fe279e2152747d
Jakub Zehner <kubusja.pl@gmail.com>
2023-08-02 17:09:48 +0200

    add csharpier

--- SensoBackend/Contracts/User/UserDto.cs
+++ SensoBackend/Contracts/User/UserDto.cs
@@ -4,6 +4,9 @@ namespace SensoBackend.Contracts.User;
 
 public record UserDto
 {
-    [Required] public required int Id { get; set; }
-    [Required] public required string Name { get; set; }
+    [Required]
+    public required int Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
 }
--- SensoBackend/Data/AppDbContext.cs
+++ SensoBackend/Data/AppDbContext.cs
@@ -6,9 +6,7 @@ namespace SensoBackend.Data;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     public DbSet<User> Users { get; set; }
 }
