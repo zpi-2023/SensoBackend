a04c857d28285a3f41848e9a774f1556c526bcd7
Jakub Zehner <kubusja.pl@gmail.com>
2023-08-05 20:16:27 +0200

    check instead of fix

--- SensoBackend/Contracts/User/UserDto.cs
+++ SensoBackend/Contracts/User/UserDto.cs
@@ -7,5 +7,6 @@ public record UserDto
     [Required]
     public required int Id { get; set; }
 
-    [Required] public required string Name { get; set; }
+    [Required]
+    public required string Name { get; set; }
 }
