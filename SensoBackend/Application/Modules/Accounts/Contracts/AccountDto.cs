a8a24c22c7da27c1e68e4184c5e56cc7f7f7275b
Jakub Zehner <kubusja.pl@gmail.com>
2023-10-12 22:49:06 +0200

    fix required fields in dto

--- SensoBackend/Application/Modules/Accounts/Contracts/AccountDto.cs
+++ SensoBackend/Application/Modules/Accounts/Contracts/AccountDto.cs
@@ -1,15 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SensoBackend.Application.Modules.Accounts.Contracts;
 
 public sealed record AccountDto
 {
+    [Required]
     public required int Id { get; init; }
+
+    [Required]
     public required string Email { get; set; }
+
+    [Required]
     public required string Password { get; set; }
+
+    [Required]
     public required bool Active { get; set; }
+
+    [Required]
     public required bool Verified { get; set; }
     public string? PhoneNumber { get; set; }
     public string? DisplayName { get; set; }
+
+    [Required]
     public required DateTimeOffset CreatedAt { get; set; }
+
+    [Required]
     public required DateTimeOffset LastLoginAt { get; set; }
+
+    [Required]
     public required DateTimeOffset LastPasswordChangeAt { get; set; }
 }
