8875f2fea262e94218f3a056ff67d79d5c4d73ef
Kamil Ciągło <kamilciag123@gmail.com>
2023-10-20 12:22:58 +0200

    Make Alias property in Profile required

--- SensoBackend/Domain/Entities/Profile.cs
+++ SensoBackend/Domain/Entities/Profile.cs
@@ -5,7 +5,7 @@ public sealed class Profile
     public required int Id { get; set; }
     public required int AccountId { get; set; }
     public required int SeniorId { get; set; }
-    public string? Alias { get; set; }
+    public required string Alias { get; set; }
 
     public Account? Account { get; set; }
     public Account? Senior { get; set; }
