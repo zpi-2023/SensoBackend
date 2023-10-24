4e644c171a01113073d8e9bef8e68cf9f52221f2
Kamil Ciągło <105738508+Daxin18@users.noreply.github.com>
2023-10-24 22:32:33 +0200

    Hotfix for profiles (#43)

--- SensoBackend/Application/Modules/Profiles/Contracts/CreateCaretakerProfileDto.cs
+++ SensoBackend/Application/Modules/Profiles/Contracts/CreateCaretakerProfileDto.cs
@@ -5,7 +5,7 @@ namespace SensoBackend.Application.Modules.Profiles.Contracts;
 public class CreateCaretakerProfileDto
 {
     [Required]
-    public required int EncodedSeniorId { get; init; }
+    public required int Hash { get; init; }
 
     [Required]
     public required string SeniorAlias { get; init; }
