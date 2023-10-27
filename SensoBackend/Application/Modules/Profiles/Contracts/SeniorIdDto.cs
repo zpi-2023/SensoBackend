b463796cbc12ae03cc6ddd53a3a696c73ca6ca9f
Jakub Zehner <kubusja.pl@gmail.com>
2023-10-27 17:25:38 +0200

    rm unused dto

--- SensoBackend/Application/Modules/Profiles/Contracts/SeniorIdDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace SensoBackend.Application.Modules.Profiles.Contracts;
-
-public class SeniorIdDto
-{
-    [Required]
-    public required int SeniorId { get; init; }
-}
