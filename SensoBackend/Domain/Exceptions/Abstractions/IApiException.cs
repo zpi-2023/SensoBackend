eb2afeed098a8fb88edbc672b9a1b1a2e82063e4
Jakub Zehner <kubusja.pl@gmail.com>
2023-11-28 22:03:24 +0100

    oops

--- SensoBackend/Domain/Exceptions/Abstractions/IApiException.cs
+++
@@ -1,6 +0,0 @@
-namespace SensoBackend.Domain.Exceptions.Abstractions;
-
-public abstract class ApiException(int statusCode, string message) : Exception(message)
-{
-    public int StatusCode { get; } = statusCode;
-}
