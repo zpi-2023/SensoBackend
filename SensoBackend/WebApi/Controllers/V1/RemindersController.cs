f53f692a934f4dc12802ff8b8e2331b886550793
Kamil Ciągło <kamilciag123@gmail.com>
2023-12-02 12:22:02 +0100

    Move reminders endpoints around

--- SensoBackend/WebApi/Controllers/V1/RemindersController.cs
+++ SensoBackend/WebApi/Controllers/V1/RemindersController.cs
@@ -65,35 +65,6 @@ public sealed class RemindersController(IMediator mediator) : ControllerBase
     }
 
     /// <summary>
-    /// Returns intakes for a reminer with a given id
-    /// </summary>
-    /// <param name="reminderId"> Id of a reminder </param>
-    /// <response code="200"> Returns paginated intakes for a reminder with a given id </response>
-    /// <response code="401"> If user is not logged in </response>
-    /// <response code="403"> If user does not have a profile needed to access this reminder </response>
-    /// <response code="404"> If reminder with given Id does not exist in the database </response>
-    [HasPermission(Permission.ManageReminders)]
-    [HttpGet("{reminderId}/intakes")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PaginatedDto<IntakeDto>))]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetAllIntakesForReminder(
-        int reminderId,
-        [FromQuery] PaginationQuery query
-    )
-    {
-        var accountId = this.GetAccountId();
-        var request = new GetAllIntakesForReminderRequest
-        {
-            AccountId = accountId,
-            ReminderId = reminderId,
-            PaginationQuery = query
-        };
-        return Ok(await mediator.Send(request));
-    }
-
-    /// <summary>
     /// Deletes reminder with a given id
     /// </summary>
     /// <param name="reminderId"> Id of a reminder </param>
@@ -118,59 +89,75 @@ public sealed class RemindersController(IMediator mediator) : ControllerBase
     }
 
     /// <summary>
-    /// Creates a new intake record for a reminder with a given id
+    /// Returns reminders for a senior with a given id
     /// </summary>
-    /// <param name="reminderId"> Id of a reminder </param>
-    /// <param name="dto"> Information about intake </param>
-    /// <response code="201"> Returns newly created intake record </response>
-    /// <response code="400"> If validation failed </response>
+    /// <param name="seniorId"> Id of a senior who's reminders are to be returned </param>
+    /// <response code="200"> Returns reminders for a senior with a given id </response>
     /// <response code="401"> If user is not logged in </response>
-    /// <response code="403"> If user does not have a profile needed to access this reminder </response>
-    /// <response code="404"> If reminder with given Id does not exist in the database </response>
+    /// <response code="403"> If user does not have a profile needed to access this senior's reminders </response>
     [HasPermission(Permission.ManageReminders)]
-    [HttpPost("{reminderId}/intakes")]
-    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(IntakeDto))]
+    [HttpGet("senior/{seniorId}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PaginatedDto<ReminderDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> GetSeniorReminders(
+        int seniorId,
+        [FromQuery] PaginationQuery query
+    )
+    {
+        var accountId = this.GetAccountId();
+        var request = new GetSeniorRemindersRequest
+        {
+            AccountId = accountId,
+            SeniorId = seniorId,
+            PaginationQuery = query
+        };
+        return Ok(await mediator.Send(request));
+    }
+
+    /// <summary>
+    /// Creates a new reminder for a senior with a given Id
+    /// </summary>
+    /// <param name="seniorId"> Id of a senior </param>
+    /// <param name="dto"> Data used to create a new reminder </param>
+    /// <response code="201"> Returns newly created reminder </response>
+    /// <response code="401"> If validation failed </response>
+    /// <response code="401"> If user is not logged in </response>
+    /// <response code="403"> If user does not have a profile needed to access this senior's reminders </response>
+    [HasPermission(Permission.ManageReminders)]
+    [HttpPost("senior/{seniorId}")]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ReminderDto))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> CreateIntakeRecord(int reminderId, CreateIntakeDto dto)
+    public async Task<IActionResult> CreateReminder(int seniorId, CreateReminderDto dto)
     {
         var accountId = this.GetAccountId();
-        var request = new CreateIntakeRequest
-        {
-            AccountId = accountId,
-            Dto = dto,
-            ReminderId = reminderId
-        };
 
-        var intakeDto = await mediator.Send(request);
+        var reminderDto = await mediator.Send(
+            new CreateReminderRequest
+            {
+                AccountId = accountId,
+                SeniorId = seniorId,
+                Dto = dto
+            }
+        );
 
         return CreatedAtAction(
-            nameof(GetIntakeRecordById),
-            new { intakeId = intakeDto.Id },
-            intakeDto
+            nameof(GetReminderById),
+            new { reminderId = reminderDto.Id },
+            reminderDto
         );
     }
 
     /// <summary>
-    /// Returns intake with a given id
+    /// Returns all medications that fit the search criteria
     /// </summary>
-    /// <param name="intakeId"> Id of an intake </param>
-    /// <response code="200"> Returns intake with a given id </response>
-    /// <response code="401"> If user is not logged in </response>
-    /// <response code="403"> If user does not have a profile needed to access this intake </response>
-    /// <response code="404"> If intake with given Id does not exist in the database </response>
-    [HasPermission(Permission.ManageReminders)]
-    [HttpGet("intakes/{intakeId}")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IntakeDto))]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetIntakeRecordById(int intakeId)
-    {
-        var accountId = this.GetAccountId();
-        var request = new GetIntakeByIdRequest { AccountId = accountId, IntakeId = intakeId };
-        return Ok(await mediator.Send(request));
-    }
+    /// <param name="search"> Search criteria (not case-sensitive) </param>
+    /// <response code="200"> Medications that fit the search criteria </response>
+    [HttpGet("medications")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PaginatedDto<MedicationDto>))]
+    public async Task<IActionResult> GetMedicationList(
+        [FromQuery(Name = "search")] string search
+    ) => Ok(await mediator.Send(new GetMedicationListRequest { SearchTerm = search }));
 }
