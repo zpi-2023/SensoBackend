75e5a9df7812811708244a0a200940ea8709844e
Jakub Zehner <kubusja.pl@gmail.com>
2023-12-07 00:20:16 +0100

    migrations

---
+++ SensoBackend/Infrastructure/Migrations/20231206231826_NewAlertAndUniqueInLeaderboardEntry.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SensoBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewAlertAndUniqueInLeaderboardEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LeaderboardEntries_AccountId",
+                table: "LeaderboardEntries");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaderboardEntries_AccountId_Game",
+                table: "LeaderboardEntries",
+                columns: new[] { "AccountId", "Game" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LeaderboardEntries_AccountId_Game",
+                table: "LeaderboardEntries");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaderboardEntries_AccountId",
+                table: "LeaderboardEntries",
+                column: "AccountId");
+        }
+    }
+}
