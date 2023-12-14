efb088e7f0772ea0ab752479fc03b163a7e27b1e
Jakub Zehner <kubusja.pl@gmail.com>
2023-12-14 20:33:07 +0100

    even more logs

--- SensoBackend/Application/RemidersScheduler/CreateCaretakerReminderAlert.cs
+++ SensoBackend/Application/RemidersScheduler/CreateCaretakerReminderAlert.cs
@@ -19,21 +19,41 @@ public sealed record CreateCaretakerReminderAlertRequest : IRequest
 public sealed class CreateCaretakerReminderAlertHandler(
     AppDbContext context,
     TimeProvider timeProvider,
-    IAlertDispatcher alertDispatcher
+    IAlertDispatcher alertDispatcher,
+    ILogger<CreateCaretakerReminderAlertHandler> logger
 ) : IRequestHandler<CreateCaretakerReminderAlertRequest>
 {
     private static readonly TimeSpan intakeThreshold = TimeSpan.FromMinutes(5);
 
     public async Task Handle(CreateCaretakerReminderAlertRequest request, CancellationToken ct)
     {
+        logger.LogInformation(
+            "Creating caretaker reminder alert for reminder {ReminderId} and senior {SeniorId}",
+            request.ReminderId,
+            request.SeniorId
+        );
+
         var lastIntake = await context
             .IntakeRecords
             .Where(r => r.ReminderId == request.ReminderId)
             .OrderByDescending(r => r.TakenAt)
             .FirstOrDefaultAsync(ct);
 
+        logger.LogInformation(
+            "Last intake for reminder {ReminderId} and senior {SeniorId} was at {LastIntake}",
+            request.ReminderId,
+            request.SeniorId,
+            lastIntake?.TakenAt
+        );
+
         if (lastIntake is null || timeProvider.GetUtcNow() - lastIntake.TakenAt > intakeThreshold)
         {
+            logger.LogInformation(
+                "Last intake for reminder {ReminderId} and senior {SeniorId} was more than {IntakeThreshold} ago - creating alert",
+                request.ReminderId,
+                request.SeniorId,
+                intakeThreshold
+            );
             var alert = new Alert
             {
                 Id = default,
@@ -43,6 +63,15 @@ public sealed class CreateCaretakerReminderAlertHandler(
             };
 
             await alertDispatcher.Dispatch(alert, ct);
+        }
+        else
+        {
+            logger.LogInformation(
+                "Last intake for reminder {ReminderId} and senior {SeniorId} was less than {IntakeThreshold} ago - alert not created",
+                request.ReminderId,
+                request.SeniorId,
+                intakeThreshold
+            );
         }
     }
 }
--- SensoBackend/Application/RemidersScheduler/CreateReminderAlert.cs
+++ SensoBackend/Application/RemidersScheduler/CreateReminderAlert.cs
@@ -21,21 +21,41 @@ public sealed class CreateReminderAlertHandler(
     TimeProvider timeProvider,
     IMediator mediator,
     IAlertDispatcher alertDispatcher,
-    IHangfireWrapper hangfireWrapper
+    IHangfireWrapper hangfireWrapper,
+    ILogger<CreateReminderAlertHandler> logger
 ) : IRequestHandler<CreateReminderAlertRequest>
 {
     private static readonly TimeSpan intakeThreshold = TimeSpan.FromMinutes(5);
 
     public async Task Handle(CreateReminderAlertRequest request, CancellationToken ct)
     {
+        logger.LogInformation(
+            "Creating reminder alert for reminder {ReminderId} and senior {SeniorId}",
+            request.ReminderId,
+            request.SeniorId
+        );
+
         var lastIntake = await context
             .IntakeRecords
             .Where(r => r.ReminderId == request.ReminderId)
             .OrderByDescending(r => r.TakenAt)
             .FirstOrDefaultAsync(ct);
 
+        logger.LogInformation(
+            "Last intake for reminder {ReminderId} and senior {SeniorId} was at {LastIntake}",
+            request.ReminderId,
+            request.SeniorId,
+            lastIntake?.TakenAt
+        );
+
         if (lastIntake is null || timeProvider.GetUtcNow() - lastIntake.TakenAt > intakeThreshold)
         {
+            logger.LogInformation(
+                "Last intake for reminder {ReminderId} and senior {SeniorId} was more than {IntakeThreshold} ago - creating alert",
+                request.ReminderId,
+                request.SeniorId,
+                intakeThreshold
+            );
             var alert = new Alert
             {
                 Id = default,
@@ -51,10 +71,23 @@ public sealed class CreateReminderAlertHandler(
 
             await alertDispatcher.Dispatch(alert, ct);
         }
+        else
+        {
+            logger.LogInformation(
+                "Last intake for reminder {ReminderId} and senior {SeniorId} was less than {IntakeThreshold} ago - not creating alert",
+                request.ReminderId,
+                request.SeniorId,
+                intakeThreshold
+            );
+        }
     }
 
     public async Task CreateReminderAlert(int reminderId, int seniorId)
     {
+        logger.LogInformation(
+            "Sending to mediator request to create alert for caretaker for reminder: {ReminderId}",
+            reminderId
+        );
         await mediator.Send(
             new CreateCaretakerReminderAlertRequest { ReminderId = reminderId, SeniorId = seniorId }
         );
